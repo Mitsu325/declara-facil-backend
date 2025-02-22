40fa8657833294d1f6c3e38c1c96e3e9e280b078
Mitsu325 <patricia.mashiba@gmail.com>
2025-02-22 14:43:31 -0300

    refactor: conectar no banco usando url

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,12 +4,11 @@ dotenvConfig({ path: '.env' });
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    url: process.env.DATABASE_URL,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     autoLoadEntities: true,
     synchronize: true,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
