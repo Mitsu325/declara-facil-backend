22c8d8a61f7994918e375b38af8e9efcacbec991
Mitsu325 <patricia.mashiba@gmail.com>
2025-02-22 15:17:50 -0300

    chore: alterar o url de cors

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://declara-facil-frontend.vercel.app',
     credentials: true,
   });
 
